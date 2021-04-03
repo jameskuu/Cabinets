e85aa0692145778da8e5b5df21850cb1d248a962
James <develop@kuusisto.com>
2021-04-02 21:03:20 -0700

    First Commit - starting UpperCabinet

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Cabinets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UpperCabinet upperCabinet = new UpperCabinet(1,2,3,4,5,6);
+            Console.WriteLine(upperCabinet.height);
+
+            
+        }
+    }
+}
---
+++ entity/UpperCabinet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cabinets
+{
+    class UpperCabinet
+    {
+        public double length { get; set; }
+        public double height { get; set; }
+        public double leftStyle { get; set; }
+        public double rightStyle { get; set; }
+        private double centerStyle { get; set; }
+        private int numCenterStyles { get; set; }
+
+        public UpperCabinet(double length, double height, double leftStyle, double rightStyle, double centerStyle, int numCenterStyles )
+        {
+            this.length = length;
+            this.height = height;
+            this.leftStyle = leftStyle;
+            this.rightStyle = rightStyle;
+            this.centerStyle = centerStyle;
+            this.numCenterStyles = numCenterStyles;
+        }
+
+    }
+}
